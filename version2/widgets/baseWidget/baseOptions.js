9f52df1837d50915f9709985f1c3f507b1c361f0
Dileep <Dileep.Ramachandrarao.Krishna.Murthy@iais.fraunhofer.de>
2017-08-08 16:58:23 +0200

    add loop symbol

--- version2/widgets/baseWidget/baseOptions.js
+++ version2/widgets/baseWidget/baseOptions.js
@@ -383,6 +383,32 @@ function BaseControls(parentWidget) {
 
     }// end of base group funcion
 
+    this.createModal = function(id, header, content) {
+        var html =  '<div id="'+id+'" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="confirm-modal" aria-hidden="true">';
+        html += '<div class="modal-dialog">';
+        html += '<div class="modal-content">';
+        html += '<div class="modal-header">';
+        html += '<a class="close" data-dismiss="modal">×</a>';
+        html += '<h4>'+header+'</h4>'
+        html += '</div>';
+        html += '<div class="modal-body">';
+        html += content;
+        html += '</div>';
+        html += '<div class="modal-footer">';
+        html += '<span class="btn btn-primary" data-dismiss="modal">Close</span>';
+        html += '</div>';  // content
+        html += '</div>';  // dialog
+        html += '</div>';  // footer
+        html += '</div>';  // modalWindow
+        $('body').append(html);
+        $("#dynamicModal").modal();
+        $("#dynamicModal").modal('show');
+
+        $('#dynamicModal').on('hidden.bs.modal', function (e) {
+            $(this).remove();
+        });
+    };
+
 
     this.handleNodeSelection=function(node){
         // should be overwritten by the real options thing
